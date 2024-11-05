b46b83f954433ce84557f33b8ed9cc2b506d26a1
cascodenotch <catalinaarciniegassaenz@gmail.com>
2024-11-05 19:14:15 +0100

    Retos adicionales

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,6 @@
 export class User {
 
-    constructor(public Id_user: number, public name: string, public last_name: string, public email: string, public photo: string, public password: string){}
+    constructor(public Id_user: number, public name: string, public last_name: string, public email: string, public photo: string, public password: string){
+    }
 
 }
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -9,27 +9,40 @@ import { User } from 'src/app/models/user';
 export class ProfileComponent {
 
   public user: User;
+  public mensaje: string;
 
   constructor (){
-    this.user =new User(1, "Catalina", "Arciniegas", "cas@gmail.com", "cas", "cas");
+    this.user =new User(1, "Catalina", "Arciniegas", "cas@gmail.com", "/assets/fotoperfil.jpg", "cas");
   }
 
   public mostrarNombre2 (){
     console.log(this.user.name)
   }
 
-  public editUser(newName:string, newLastname: string, newEmail:string){
-
+  public editUser(newName:string, newLastname: string, newEmail:string, newPhoto: string){
+  
     if(newName != ""){
       this.user.name = newName;
+      this.mensaje = "Usuario actualizado"
     }
 
     if (newLastname !=""){
       this.user.last_name = newLastname;
+      this.mensaje = "Usuario actualizado"
     }
     
     if (newEmail != ""){
     this.user.email = newEmail; 
+     this.mensaje = "Usuario actualizado"
+    }
+
+    if(newPhoto!= ""){
+      this.user.photo =newPhoto;
+      this.mensaje = "Usuario actualizado"
+    }
+
+    if ((newPhoto=="") && (newEmail== "")&& (newLastname=="")&& (newName== "")){
+    this.mensaje = "No se detectan cambios"
     }
   }
 }
